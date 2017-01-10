ddce5f30ae12499095c00211cebe4c97cc8d9c72
Patrick Vogt <patrick.vogt@unibas.ch>
2017-01-10 19:21:12 +0100

    unit testing

--- app/src/test/java/ch/almana/calctrainer/ExampleUnitTest.java
+++ app/src/test/java/ch/almana/calctrainer/ExampleUnitTest.java
@@ -1,6 +1,9 @@
 package ch.almana.calctrainer;
 
+import android.util.Log;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
 
@@ -9,9 +12,36 @@ import static org.junit.Assert.*;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
+
 public class ExampleUnitTest {
+
     @Test
     public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+        int min = 3;
+        int max = 50;
+        ICalulcationBuilder calculationBuilder = new PlusCalculationBuilder(min,max);
+        for (int i = 0; i < max; i++){
+            checkBounds("plus ",calculationBuilder,min,max);
+        }
+    }
+
+    @Test
+    public void subtraction_isCorrect() throws Exception {
+        int min = 3;
+        int max = 50;
+        ICalulcationBuilder calculationBuilder = new MinusCalculationBuilder(min,max);
+        for (int i = 0; i < max; i++){
+            checkBounds("minus ",calculationBuilder,min,max);
+        }
+    }
+
+    private void checkBounds(String msg, ICalulcationBuilder calculationBuilder, int min, int max) {
+        calculationBuilder.build();
+        assertTrue(calculationBuilder.toString(),Integer.parseInt(String.valueOf(calculationBuilder.getNumber1())) >= min);
+        assertTrue(calculationBuilder.toString(),Integer.parseInt(String.valueOf(calculationBuilder.getNumber1())) <= max);
+        assertTrue(calculationBuilder.toString(),Integer.parseInt(String.valueOf(calculationBuilder.getNumber2())) >= min);
+        assertTrue(calculationBuilder.toString(),Integer.parseInt(String.valueOf(calculationBuilder.getNumber2())) <= max);
+        assertTrue(calculationBuilder.toString(),Integer.parseInt(String.valueOf(calculationBuilder.getResult())) >= min);
+        assertTrue(calculationBuilder.toString(),Integer.parseInt(String.valueOf(calculationBuilder.getResult())) <= max);
     }
 }
